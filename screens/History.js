a6ca36fa55577b5810956d4c4c9d7df7dc5d28f4
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2022-08-04 01:25:13 +0200

    Add header to history screen

--- screens/History.js
+++ screens/History.js
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList, StyleSheet} from 'react-native';
-import {Appbar, Card, Paragraph, Title} from 'react-native-paper';
+import {
+  Appbar,
+  Card,
+  Paragraph,
+  Title,
+  Divider,
+  IconButton,
+  Colors,
+} from 'react-native-paper';
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
 import {getAllResults} from '../src/services';
 
@@ -9,16 +17,46 @@ const {s} = bootstrapStyleSheet;
 
 const History = () => {
   const [allResults, setAllResults] = useState([]);
+  const [sortBy, setSortBy] = useState('latest');
 
   const styles = StyleSheet.create({
     appBg: {
       backgroundColor: '#F8F8FA',
     },
+    menuContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      display: 'flex',
+    },
   });
 
-  useEffect(() => {
+  const toggleSortBy = () => {
+    if (sortBy === 'latest') {
+      setSortBy('oldest');
+    } else {
+      setSortBy('latest');
+    }
+  };
+
+  const refresh = () =>
     getAllResults().then(AllResults => setAllResults(AllResults));
-  }, []);
+
+  useEffect(() => refresh(), []);
+  useEffect(() => {
+    if (sortBy === 'latest') {
+      setAllResults(
+        allResults.sort((a, b) => {
+          return new Date(a.date) < new Date(b.date);
+        }),
+      );
+    } else {
+      setAllResults(
+        allResults.sort((a, b) => {
+          return new Date(a.date) > new Date(b.date);
+        }),
+      );
+    }
+  }, [sortBy, allResults]);
 
   return (
     <View style={styles.appBg}>
@@ -29,6 +67,31 @@ const History = () => {
       <View style={[s.container, s.h90]}>
         <FlatList
           data={allResults}
+          ListHeaderComponent={
+            <>
+              <View style={[styles.menuContainer, s.mt2, s.mx2]}>
+                <IconButton
+                  style={s.m0}
+                  icon="refresh"
+                  size={31}
+                  color={Colors.grey600}
+                  onPress={() => refresh()}
+                />
+                <IconButton
+                  style={s.m0}
+                  icon={
+                    sortBy === 'latest'
+                      ? 'sort-clock-descending'
+                      : 'sort-clock-ascending'
+                  }
+                  size={31}
+                  color={Colors.grey600}
+                  onPress={() => toggleSortBy()}
+                />
+              </View>
+              <Divider />
+            </>
+          }
           ListEmptyComponent={
             <Title style={[s.textMuted, s.textCenter]}>
               L'historique est vide
